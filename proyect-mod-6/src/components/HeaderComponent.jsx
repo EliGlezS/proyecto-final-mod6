d13e4c2d916925f629bb608803d44d4d21e1db04
tinadanit <ifaraoropesa@gmail.com>
2025-03-18 23:05:20 +0000

    header para movil

--- proyect-mod-6/src/components/HeaderComponent.jsx
+++ proyect-mod-6/src/components/HeaderComponent.jsx
@@ -14,7 +14,11 @@ const HeaderComponent = () => {
         <div>
             <header>
                 <div className = "burger">
-                    <button onClick= {toggleMenu} className='boton_menu' id='x'>X</button>
+                    <button onClick= {toggleMenu} className='boton_menu' id='x'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                    </button>
                 </div>
                 <div className="logo-container">
                     <a href="">
@@ -28,16 +32,6 @@ const HeaderComponent = () => {
                         <li><a href="/">Eventos</a></li>
                     </ul>  
                 </nav>
-                <div className="login-container">
-                    <a href="/">
-                        Mi cuenta
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
-                        </div>
-                    </a>
-                </div>
                 <div className="search-container">
                     <input type="search" id="menu-search" placeholder="¿Qué libro buscas?"></input> 
                     <div className="cart-img">
@@ -46,13 +40,23 @@ const HeaderComponent = () => {
                         </svg>
                     </div>
                 </div>
+                <div className="login-container">
+                    <a href="/">
+                        <p>Mi cuenta</p>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                        </div>
+                    </a>
+                </div>
                 <div className="cart-container">
                     <a href="">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                         </svg>
                     </a>
-                    <span className="cart-number"></span>
+                    <span className="cart-number">0</span>
                 </div>
             </header>
         </div>
