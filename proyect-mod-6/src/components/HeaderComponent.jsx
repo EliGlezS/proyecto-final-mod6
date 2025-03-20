c47a624eee47172027ec46a1906cdf9893763727
tinadanit <ifaraoropesa@gmail.com>
2025-03-20 19:53:11 +0000

    borrada primera version

--- proyect-mod-6/src/components/HeaderComponent.jsx
+++ proyect-mod-6/src/components/HeaderComponent.jsx
@@ -1,67 +1,63 @@
-import "../styles/headerComponent.css"
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import SvgLogo from "../img/logos/LuzDeTinta-completo-v5.svg";
+import "../styles/headerStyle.css";
 
-const HeaderComponent = () => {
+const Header = () => {
+    const [showNav, setShowNav] = useState(false);
 
-    const toggleMenu = () =>{
-        let menu_desplegable = document.getElementById('menu');
-        let boton_cerrar = document.getElementById('x');
-        menu_desplegable.classList.toggle('abrir_menu');
-        boton_cerrar.classList.toggle('colocar_x');
-    };
+    const handleShowNav = () => {
+      setShowNav(!showNav);
+    }
 
     return (
-        <>
-        <div>
-            <header>
-                <div className = "burger">
-                    <button onClick= {toggleMenu} className='boton_menu' id='x'>
+        <header>
+            <div className="login-container">
+                <NavLink to="/login" element="LoginComponent">
+                    <p></p>
+                    <div className="icon-container">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                        </svg>
-                    </button>
-                </div>
-                <div className="logo-container">
-                    <a href="">
-                        <svg src="../img/LuzDeTinta modelo nitido (1).svg" alt="logo"></svg>
-                    </a>
-                </div>
-                <nav id='header-nav' className="header-nav">
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/">Catálogo</a></li>
-                        <li><a href="/">Eventos</a></li>
-                    </ul>  
-                </nav>
-                {/* <div className="search-container">
-                    <input type="search" id="menu-search" placeholder="¿Qué libro buscas?"></input> 
-                    <div className="cart-img">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
                     </div>
-                </div> */}
-                <div className="login-container">
-                    <a href="/">
-                        <p></p>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
-                        </div>
-                    </a>
-                </div>
-                <div className="cart-container">
-                    <a href="">
+                </NavLink>
+            </div>
+            <div className="logo-container">
+                <NavLink to="/">
+                    <div className="image-logo-container">
+                        <img src={SvgLogo} alt="logotipo"/>
+                    </div>
+                </NavLink>
+            </div>
+            <div className="cart-container">
+                <NavLink to="/cart" element="ShoppingCart">
+                    <div className="icon-container">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                         </svg>
-                    </a>
-                    <span className="cart-number">0</span>
-                </div>
-            </header>
-        </div>
-       </>
-   );
+                    </div>
+                </NavLink>
+                <span className="cart-number">0</span>
+            </div>
+            <div className = "burger-container">
+                <button className='icon-container' id='burger' onClick={handleShowNav}>
+                    {!showNav ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                    : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>}
+                </button>
+            </div>
+            <nav id='header-nav' className={`header-nav  ${showNav && "active"}`}>
+                <ul>
+                    <li><NavLink to="/" element="Home">Home</NavLink></li>
+                    <li><NavLink to="/recommended" element="Recommended">Recomendados</NavLink></li>
+                    <li><NavLink to="/events">Eventos</NavLink></li>
+                </ul>  
+            </nav>
+        </header>
+    )
 }
-
-export default HeaderComponent
+    
+export default Header;
