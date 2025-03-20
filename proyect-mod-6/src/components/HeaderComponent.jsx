0bf8ecf7d1d0ca3097408baea66fea170875acd3
tinadanit <ifaraoropesa@gmail.com>
2025-03-20 23:33:42 +0000

    estilado y cambios responsive-pruebas

--- proyect-mod-6/src/components/HeaderComponent.jsx
+++ proyect-mod-6/src/components/HeaderComponent.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useMediaQuery} from "../customHook/useMediaQuery"
 import SvgLogo from "../img/logos/LuzDeTinta-completo-v5.svg";
 import "../styles/headerStyle.css";
 
 const Header = () => {
+
     const [showNav, setShowNav] = useState(false);
+    
+    const isMobile = useMediaQuery("max-width: 768px");
 
     const handleShowNav = () => {
-      setShowNav(!showNav);
+        setShowNav(!showNav);
     }
-
+    
     return (
         <header>
             <div className="login-container">
                 <NavLink to="/login" element="LoginComponent">
-                    <p></p>
                     <div className="icon-container">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -39,7 +42,7 @@ const Header = () => {
                 </NavLink>
                 <span className="cart-number">0</span>
             </div>
-            <div className = "burger-container">
+            <div className="burger-container">
                 <button className='icon-container' id='burger' onClick={handleShowNav}>
                     {!showNav ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -48,11 +51,12 @@ const Header = () => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                     </svg>}
                 </button>
-            </div>
-            <nav id='header-nav' className={`header-nav  ${showNav && "active"}`}>
+            </div>            
+            <nav className={isMobile ? `header-nav ${showNav && "active"}` : `header-nav-desktop`}>
+            {/* <nav className={`header-nav ${showNav && "active"}`}> */}
                 <ul>
-                    <li><NavLink to="/" element="Home">Home</NavLink></li>
-                    <li><NavLink to="/recommended" element="Recommended">Recomendados</NavLink></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/recommended">Recomendados</NavLink></li>
                     <li><NavLink to="/events">Eventos</NavLink></li>
                 </ul>  
             </nav>
