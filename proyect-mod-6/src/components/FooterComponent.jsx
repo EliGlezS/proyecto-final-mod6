7531eb576cb3071ade595c779a25c0066381f95c
tinadanit <ifaraoropesa@gmail.com>
2025-03-23 16:03:48 +0000

    estilado footer desktop

--- proyect-mod-6/src/components/FooterComponent.jsx
+++ proyect-mod-6/src/components/FooterComponent.jsx
@@ -9,26 +9,26 @@ import LogoFooter from '../img/logos/LuzDeTinta-letras-negro.svg'
 
 const Footer = () => {
 
-    // const isDesktop = useMediaQuery('(min-width: 769px)');
+    // const isDesktop = useMediaQuery('(min-width: 769px)')
 
     return (
         <footer>
             <section className='newsletter'>
                 <div className='newsletter-container'>
-                <div className='newsletter-icon'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                    </svg>
-                </div>
-                <form>
-                    <h3>Suscríbete a nuestro boletín</h3>
-                    <input type='email' placeholder='Escribe tu email' required className='newsletter-email'></input>
-                    <button type='submit' className='newsletter-button'>Enviar</button>
-                    <label>
-                        <input type='checkbox' className='newsletter-checkbox'></input>
-                        Acepto la política de privacidad
-                    </label>
-                </form>
+                    <div className='newsletter-icon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                        </svg>
+                    </div>
+                    <form>
+                        <h3>Suscríbete a nuestro boletín</h3>
+                        <input type='email' placeholder='Escribe tu email' required className='newsletter-email'></input>
+                        <button type='submit' className='newsletter-button'>Enviar</button>
+                        <label>
+                            <input type='checkbox' className='newsletter-checkbox'></input>
+                            Acepto la política de privacidad
+                        </label>
+                    </form>
                 </div>
             </section>
             <section className='footer-info'>
@@ -84,6 +84,8 @@ const Footer = () => {
                     </li>
                 </ul>
                 </nav>
+            </section>
+            <section className='copyright-container'>
                 <div className='copyright'>
                 <small>&copy; Luz de Tinta 2025</small>
                 </div>
