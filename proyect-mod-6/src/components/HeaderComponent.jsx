3b34cf6ad32a794d6a487d0e24d00b94788ddfca
tinadanit <ifaraoropesa@gmail.com>
2025-03-26 12:39:12 +0000

    cambio enlace login con la sesion iniciada

--- proyect-mod-6/src/components/HeaderComponent.jsx
+++ proyect-mod-6/src/components/HeaderComponent.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useMediaQuery} from "../customHook/useMediaQuery"
 import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 import { ProductsCartContext } from "../context/ProductsCartContext";
 import SvgLogo from "../img/logos/LuzDeTinta-completo-v5.svg";
 import "../styles/headerStyle.css";
@@ -16,10 +17,12 @@ const Header = () => {
         setShowNav(!showNav);
     } 
 
+    const { isLoggedIn } = useContext(UserContext);
+
     return (
         <header>
             <div className="login-container">
-                <NavLink to="/login" element="LoginComponent">
+                <NavLink to={!isLoggedIn ? `/login` : `/perfil` }>
                     <div className="icon-container" title="Mi cuenta">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
