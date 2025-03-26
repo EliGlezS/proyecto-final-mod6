10be076649eea3f9a97d9a0b07798736236d574c
tinadanit <ifaraoropesa@gmail.com>
2025-03-26 14:00:37 +0000

    imagen de usuario con sesion iniciada en header

--- proyect-mod-6/src/components/HeaderComponent.jsx
+++ proyect-mod-6/src/components/HeaderComponent.jsx
@@ -9,6 +9,8 @@ import "../styles/headerStyle.css";
 
 const Header = () => {
 
+    // Custom hook useMediaQuery para mostrar o no el menú dentro del header
+
     const isDesktop = useMediaQuery('(min-width: 768px)');
     
     const [showNav, setShowNav] = useState(false);
@@ -17,16 +19,30 @@ const Header = () => {
         setShowNav(!showNav);
     } 
 
-    const { isLoggedIn } = useContext(UserContext);
+    // Contexto para saber si está iniciada la sesión
+
+    const { isLoggedIn, authUser } = useContext(UserContext);
+
+    const [photo, setPhoto]= useState(authUser.foto);
+
+    useEffect(()=>{
+        if (authUser) {
+          setPhoto(authUser.foto);
+        }
+      },[authUser]);
+
 
     return (
         <header>
             <div className="login-container">
                 <NavLink to={!isLoggedIn ? `/login` : `/perfil` }>
                     <div className="icon-container" title="Mi cuenta">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        </svg>
+                        </svg> */}
+                        {!isLoggedIn ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg> : <img className="user-img" src={authUser.foto} alt={authUser.nombre.charAt(0)}/>}
                     </div>
                 </NavLink>
             </div>
