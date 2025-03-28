38689d5f5487e94597429068d4f63c269befae4f
EliGlez <jelizabethglezs@gmail.com>
2025-03-28 19:52:08 +0000

    Cambios de estilado en botón (Ver más)

--- proyect-mod-6/src/components/EventsComponent.jsx
+++ proyect-mod-6/src/components/EventsComponent.jsx
@@ -82,7 +82,11 @@ const EventsComponent = () => {
             <p><strong>Fecha:</strong> {event.date}</p>
 
             <button onClick={() => setExpandedEvent(expandedEvent === event.id ? null : event.id)}>
-              {expandedEvent === event.id ? 'Ver menos' : 'Ver más'}
+              {expandedEvent === event.id ? <span>Ver menos <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+                </svg></span> : <span>Ver más <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg></span>}
             </button>
 
             {expandedEvent === event.id && (
