d43f62a465d55473dff155046f7518c4b35fd928
tinadanit <ifaraoropesa@gmail.com>
2025-03-29 15:58:35 +0000

    cierre menu movil al abrir otros enlaces header

--- proyect-mod-6/src/components/HeaderComponent.jsx
+++ proyect-mod-6/src/components/HeaderComponent.jsx
@@ -37,7 +37,7 @@ const Header = () => {
     return (
         <header>
             <div className="login-container">
-                <NavLink to={!isLoggedIn ? `/login` : `/perfil` }>
+                <NavLink to={!isLoggedIn ? `/login` : `/perfil` } onClick={closeNav}>
                     <div className="icon-container" title="Mi cuenta">
                         {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -48,14 +48,14 @@ const Header = () => {
                     </div>
                 </NavLink>
             </div>
-            <div className="logo-container">
+            <div className="logo-container" onClick={closeNav}>
                 <NavLink to="/">
                     <div className="image-logo-container" title="Luz de Tinta">
                         <img src={SvgLogo} alt="logotipo"/>
                     </div>
                 </NavLink>
             </div>
-            <div className="cart-container">
+            <div className="cart-container" onClick={closeNav}>
                 <NavLink to="/cart" element="ShoppingCart">
                     <div className="icon-container" title="Ver carrito">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
