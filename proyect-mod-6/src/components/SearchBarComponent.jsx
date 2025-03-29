c77b687052cb08f8c84cdf8b5b17d14989e28bf3
tinadanit <ifaraoropesa@gmail.com>
2025-03-29 15:49:55 +0000

    boton clear en searchbar y estilado

--- proyect-mod-6/src/components/SearchBarComponent.jsx
+++ proyect-mod-6/src/components/SearchBarComponent.jsx
@@ -23,10 +23,17 @@ const SearchBarComponent = () => {
         e.preventDefault();
     };
 
+    const handleClick = (e) => {
+        e.preventDefault();
+        setQuery("");
+        handleSearch("");
+    };
+
     return (
-        <div className='searchbar-container'>
+        <section className='searchbar-container'>
             <form className='searchbar-form' onSubmit={handleSubmit}>
                 <div className='searchbar-search'>
+                    <div className='searchbar-icon'>Buscar</div>
                     <input 
                         className='searchbar-input' 
                         type="search" 
@@ -34,7 +41,11 @@ const SearchBarComponent = () => {
                         value={query} 
                         onChange={handleSearchChange}
                     />
-                    <div className='searchbar-button'>Buscar</div>
+                    <button className='searchbar-clear' onClick={handleClick}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </button>
                 </div>
                 <div className='searchbar-check'>
                     <label className='searchbar-label'>
@@ -48,7 +59,7 @@ const SearchBarComponent = () => {
                     </label>
                 </div>
             </form>
-        </div>
+        </section>
     );
 }
 
